ad16508c059ac9de2986428196bb35479b47a05d
Knud Rishøj <tirdyr@tobenski.dk>
2023-04-09 22:43:42 +0200

    create header and image sections of the post component

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { EllipsisHorizontalIcon as Dots } from '@heroicons/react/24/solid'
+
 const Post = ({
     post: { 
         id, 
@@ -14,8 +17,21 @@ const Post = ({
             caption: string,
          }}) => {
     return (
-        <div>
-            <h1>{username}</h1>
+        <div className="bg-white my-7 border rounded-md">
+            {/* Post Header */}
+            <div className="flex items-center p-5">
+                <Image 
+                    src={userImg} 
+                    height={48} 
+                    width={48} 
+                    alt={username}
+                    className="rounded-full object-cover border p-1 mr-3"
+                    />
+                <p className="font-bold flex-1">{username}</p>
+                <Dots className="h-5" />
+            </div>
+            {/* Post image */}
+            <img src={image} alt="" className="object-cover w-full" />
         </div>
     );
 }
